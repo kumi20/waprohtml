c965412d5143001293e1c951ea74d4bdaeb80b80
Jakub Kumięga <jkumiega@csgroup.pl>
2020-12-16 10:12:41 +0100

    porawki po mergu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,13 @@ import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
 
 const routes: Routes = [
+  { path: "", component: LogOnComponent },
   { path: "test1", component: StartComponent, children:[
     { path: "", component: Test1Component, outlet: 'panel' },
   ]},
   { path: "test2", component: StartComponent, children:[
     { path: "", component: Test2Component, outlet: 'panel' },
-  ]}  { path: "", component: LogOnComponent },
+  ]},
   { path: 'articles', component: ArtykulyComponent},
   { path: 'documents', component: DokumentyMagazynoweComponent}
 ];
